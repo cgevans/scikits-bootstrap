8ae5daf32846044a706291519d2ded6eece0cd62
Constantine Evans <cevans@evanslabs.org>
2012-08-08 00:00:49 -0700

    * Wrote README * Changed alpha to allow for arbitrary number of confidence   interval percentiles. * Wrote ABC function.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 scikits-bootstrap
 =================
 
-Bootstrap Scikit for bootstrap confidence interval estimation.
+Scikits.bootstrap provides bootstrap confidence interval algorithms for scipy.
+
+At present, it is rather feature-incomplete and in flux. However, the functions
+that have been written should be relatively stable as far as results.
+
+Much of the code has been written based off the descriptions from Efron and
+Tibshirani's Introduction to the Bootstrap, and results should match the results
+obtained from following those explanations. However, the current ABC code is
+based off of the modified-BSD-licensed R port of the Efron bootstrap code, as
+I do not believe I currently have a sufficient understanding of the ABC method
+to write the code independently.
+
+In any case, please contact me (Constantine Evans <cevans@evanslabs.org>) with
+any questions or suggestions. I'm trying to add documentation, and will
+be adding tests as well. I'm especially interested, however, in how the API
+should actually look; please let me know if you think the package should be
+organized differently.
+
+The package is licensed under the Modified BSD License.
--- scikits/bootstrap/bootstrap.py
+++ scikits/bootstrap/bootstrap.py
@@ -18,8 +18,11 @@ data: array_like, shape (N, ...)
 statfunction: function
     This function should accept samples of data from ``data``. It is applied
     to these samples individually.
-alpha: float, optional
-    The percentile to use for the confidence interval (default=0.05)
+alpha: float or iterable, optional
+    The percentiles to use for the confidence interval (default=0.05). If this
+    is a float, the returned values are (alpha/2, 1-alpha/2) percentile confidence
+    intervals. If it is an iterable, alpha is assumed to be an iterable of
+    each desired percentile.
 n_samples: float, optional
     The number of bootstrap samples to use (default=10000)
 method: string
@@ -27,27 +30,34 @@ method: string
 
 Returns
 -------
-(lower, upper): tuple of floats
+confidences: tuple of floats
+    The confidence percentiles specified by alpha
 
 Calculation Methods
 -------------------
-'pi': Percentile Interval
-    The percentile interval method simply returns the 100*alpha and
-    100*(1-alpha)th bootstrap sample's values for the statistic. See
-    Efron, section 13.3.
+'pi': Percentile Interval (Efron 13.3)
+    The percentile interval method simply returns the 100*alphath bootstrap
+    sample's values for the statistic.
 
     This is an extremely simple method of confidence interval calculation.
     However, it has several disadvantages compared to the bias-corrected
     accelerated method, which is the default.
 'bca': Bias-Corrected Accelerated Non-Parametric (Efron 14.3) (default)
-    This method is much more complex to explain. However, ... (FIXME)
-'abc': Approximate Bootstrap Confidence (Efron 14.4)
-    This method is not yet implemented.
+    This method is much more complex to explain. However, it gives considerably
+    better results, and is generally recommended for normal situations. Note
+    that in cases where the statistic is smooth, and can be expressed with
+    weights, the ABC method will give approximated results much, much faster.
 
 References
 ----------
 Efron, An Introduction to the Bootstrap. Chapman & Hall 1993
     """
+    
+    # Deal with the alpha values
+    if np.iterable(alpha):
+        alphas = alpha
+    else:
+        alphas = np.array([alpha/2,1-alpha/2])
 
     # We don't need to generate actual samples; that would take more memory.
     # Instead, we can generate just the indexes, and then apply the statfun
@@ -77,17 +87,86 @@ Efron, An Introduction to the Bootstrap. Chapman & Hall 1993
         # Acceleration value
         a = np.sum( (jstat - jmean)**3 ) / ( 6.0 * np.sum( (jstat - jmean)**2 )**1.5 )
         
-        zp = z0 + norm.ppf(1-alpha/2)
-        zm = z0 - norm.ppf(1-alpha/2)
+        zs = z0 + norm.ppf(alphas)
 
-        a1 = norm.cdf(z0 + zm/(1-a*zm))
-        a2 = norm.cdf(z0 + zp/(1-a*zp))
+        avals = norm.cdf(z0 + zs/(1-a*zs))
 
-        return (stat_sorted[np.round(n_samples*a1)],stat_sorted[np.round(n_samples*a2)])
-        
+        return stat_sorted[np.round(n_samples*avals).astype('int')]    
+    
     else:
         raise ValueError()
+
+def ci_abc(data, stat, alpha, epsilon = 0.001):    
+    """
+Given a set of data ``data``, and a statistics function ``statfunction`` that
+applies to that data, computes the non-parametric approximate bootstrap 
+confidence (ABC) interval for ``stat`` on that data. Data points are assumed
+to be delineated by axis 0.
+
+Parameters
+----------
+data: array_like, shape (N, ...)
+    Input data. Data points are assumed to be delineated by axis 0. Beyond this,
+    the shape doesn't matter, so long as ``statfunction`` can be applied to the
+    array.
+stat: function (data, weights) -> value
+    The _weighted_ statistic function. This must accept weights, unlike for other
+    methods.
+alpha: float or iterable, optional
+    The percentiles to use for the confidence interval (default=0.05). If this
+    is a float, the returned values are (alpha/2, 1-alpha/2) percentile confidence
+    intervals. If it is an iterable, alpha is assumed to be an iterable of
+    each desired percentile.
+epsilon: float
+    The step size for finite difference calculations. (default=0.001)
+    
+Returns
+-------
+confidences: tuple of floats
+    The confidence percentiles specified by alpha
+
+References
+----------
+Efron, An Introduction to the Bootstrap. Chapman & Hall 1993
+bootstrap R package: http://cran.r-project.org/web/packages/bootstrap/
+    """
+    # Deal with the alpha values
+    if not np.iterable(alpha):
+        alphas = np.array([alpha/2,1-alpha/2])
+    
+    n = data.shape[0]*1.0
+    nn = data.shape[0]
+    
+    I = np.identity(nn)
+    ep = epsilon / n*1.0
+    p0 = np.repeat(1.0/n,nn)
+    
+    t1 = np.zeros(nn); t2 = np.zeros(nn)
+    t0 = stat(data,p0)
+    
+    # There MUST be a better way to do this!
+    for i in range(0,nn):
+        di = I[i] - p0
+        tp = stat(data,p0+ep*di)
+        tm = stat(data,p0-ep*di)
+        t1[i] = (tp-tm)/(2*ep)
+        t2[i] = (tp-2*t0+tm)/ep**2
+    
+    sighat = np.sqrt(np.sum(t1**2))/n
+    a = (np.sum(t1**3))/(6*n**3*sighat**3)
+    delta = t1/(n**2*sighat)
+    cq = (stat(data,p0+ep*delta)-2*t0+stat(data,p0-ep*delta))/(2*sighat*ep**2)
+    bhat = np.sum(t2)/(2*n**2)
+    curv = bhat/sighat-cq
+    z0 = norm.ppf(2*norm.cdf(a)*norm.cdf(-curv))
+    Z = z0+norm.ppf(alpha)
+    za = Z/(1-a*Z)**2
+    stan = t0 + sighat * norm.ppf(alpha)
+    abc = np.zeros_like(alpha)
+    for i in range(0,len(alpha)):
+        abc[i] = stat(data,p0+za[i]*delta)
         
+    return abc    
 
 def bootstrap_indexes(data, n_samples=10000):
     """
